c5da697cc86c2b0311fc50e6bd265517b44e8175
AEnjoy <aenjoyable@163.com>
2025-09-07 17:37:40 +0800

    refactor MinIO client initialization to use configuration struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,11 +10,14 @@ import (
 type Config struct {
 	Database `yaml:"database"`
 	Redis    `yaml:"redis"`
+	MinIO    `yaml:"minio"`
 
 	JWTSecret       string `yaml:"jwt_secret" env:"JWT_SECRET"`
 	ServerPort      string `yaml:"server_port" env:"SERVER_PORT" envDefault:"8085"`
 	WechatAppID     string `yaml:"wechat_app_id" env:"WECHAT_APP_ID"`
 	WechatAppSecret string `yaml:"wechat_app_secret" env:"WECHAT_APP_SECRET" envDefault:""`
+
+	Domain string `yaml:"domain" env:"DOMAIN" envDefault:"http://localhost:8085"`
 }
 
 // GlobalConfig is a singleton instance of Config that can be accessed globally.
@@ -32,6 +35,12 @@ type Redis struct {
 	RedisPort     int    `yaml:"redis_port" env:"REDIS_PORT" envDefault:"6379"`
 	RedisPassword string `yaml:"redis_password" env:"REDIS_PASSWORD" envDefault:""`
 	RedisDB       int    `yaml:"redis_db" env:"REDIS_DB" envDefault:"0"`
+}
+type MinIO struct {
+	MinIOEndpoint  string `yaml:"minio_endpoint" env:"MINIO_ENDPOINT" envDefault:"localhost:9000"`
+	MinIOAccessKey string `yaml:"minio_access_key" env:"MINIO_ACCESS_KEY" envDefault:"minioadmin"`
+	MinIOSecretKey string `yaml:"minio_secret_key" env:"MINIO_SECRET_KEY" envDefault:"minioadmin"`
+	MinIOUseSSL    bool   `yaml:"minio_use_ssl" env:"MINIO_USE_SSL" envDefault:"false"`
 }
 
 var _once sync.Once
