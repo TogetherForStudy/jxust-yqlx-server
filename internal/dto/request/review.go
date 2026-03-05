5d06fca9e63c858dfd063bfa7e07d1ec33319e49
Benins <sealt@qq.com>
2026-03-05 16:08:04 +0800

    fix: 放宽选课评价文本长度 docs: 更新接口文档 chore: update gitignore

--- internal/dto/request/review.go
+++ internal/dto/request/review.go
@@ -7,6 +7,6 @@ type CreateReviewRequest struct {
 	TeacherName string                 `json:"teacher_name" binding:"required"`
 	Campus      string                 `json:"campus" binding:"required"`
 	CourseName  string                 `json:"course_name" binding:"required"`
-	Content     string                 `json:"content" binding:"required,max=200"`
+	Content     string                 `json:"content" binding:"required,max=500"`
 	Attitude    models.TeacherAttitude `json:"attitude" binding:"required,oneof=1 2 3"`
 }
